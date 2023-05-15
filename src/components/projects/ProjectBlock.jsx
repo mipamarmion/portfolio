fe0d956fe982531e0bfa991b669d9949124d365f
unknown <mipamarmion@gmail.com>
2023-05-15 15:13:16 -0500

    links

--- src/components/projects/ProjectBlock.jsx
+++ src/components/projects/ProjectBlock.jsx
@@ -6,17 +6,17 @@ export default class ProjectBlock extends Component {
     render() {
         return (
           <aside style={{width: "68rem"}} className="pt-80 text-white items-center sticky h-screen w-96 p-4 space-y-4 font-mono">
-              <div style={{width: "58rem"}} className="border-4 place-content-center grid grid-cols-3 border-green-800 rounded-sm">
+              <div style={{width: "58rem"}} className="border-4 place-content-center grid grid-cols-4 border-green-800 rounded-sm">
                   <div className="flex"><img class="m-10 w-46 h-36" src={plasma}></img></div>
                   <div className="flex p-10 text-xl place-self-center">Reselling Dashboard</div>
                   <div className="flex p-10 text-m">A Dashboard and Tool for viewing analytics of items sold on Ebay, Shopify, and Amazon.</div>
-                  <div className="flex p-10 text-m"><a className="text-green" href="https://github.com/mipamarmion/bozo-scanner-client">Github Repo.</a></div>
+                  <div className="flex p-10 text-m"><a className="text-red-500" href="https://github.com/mipamarmion/bozo-scanner-client">Github Repo.</a></div>
               </div>
-              <div style={{width: "58rem"}} className="border-4 place-content-center grid grid-cols-3 border-green-800 rounded-sm">
+              <div style={{width: "58rem"}} className="border-4 place-content-center grid grid-cols-4 border-green-800 rounded-sm">
                   <div className="flex"><img class="m-10 w-46 h-36" src={retro}></img></div>
                   <div className="flex p-10 text-xl place-self-center">Open AI Retro Competition</div>
                   <div className="flex p-10 text-m">A simple artificial inteligence that is able to work its way through Sonic the Hedgehog NES original levels.</div>
-                  <div className="flex p-10 text-m"><a className="text-green" href="https://s3-us-west-2.amazonaws.com/openai-assets/research-covers/retro-contest/gotta_learn_fast_report.pdf">Program Information</a></div>
+                  <div className="flex p-10 text-m"><a className="text-red-500" href="https://s3-us-west-2.amazonaws.com/openai-assets/research-covers/retro-contest/gotta_learn_fast_report.pdf">Program Information</a></div>
               </div>
           </aside>
         )
